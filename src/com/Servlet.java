82a2285b9d3cd22d5464c520282b3d3fcc212ce3
FaizaHuma <fhuma46@gmail.com>
2018-03-23 02:28:47 +0600

    servlet_commit1

---
+++ src/com/Servlet.java
@@ -0,0 +1,40 @@
+package com;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+@WebServlet("/helloServlet")
+public class Servlet extends HttpServlet {
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+        response.setContentType("text/html");//setting the content type
+        PrintWriter pw=response.getWriter();//get the stream to write the data
+
+//writing html in the stream
+        pw.println("<html><body>");
+        pw.println("<h1>Welcome to servlet</h1>");
+
+        pw.println("</body></html>");
+
+
+
+
+
+        // Set response content type
+
+
+
+
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: User
+  Date: 3/23/2018
+  Time: 12:13 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Hello JSP file</title>
+  </head>
+  <body>
+  <h1>This is A Simple JSP Page</h1>
+
+  <p>To Continue click Here-><a href="/helloServlet">go</a></p>
+
+  $END$
+  </body>
+</html>
